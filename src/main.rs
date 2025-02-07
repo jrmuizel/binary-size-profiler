817eec7a62e5a8d01cc5bce274c22f48a29697f9
Markus Stange <markus.stange@gmail.com>
2025-02-07 14:45:27 -0500

    Move code into a process_binary function.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use fxprof_processed_profile::{
     CategoryHandle, CpuDelta, Frame, FrameFlags, FrameInfo, LibraryInfo, Profile,
     ReferenceTimestamp, SamplingInterval, StackHandle, Timestamp, WeightType,
 };
+use fxprof_processed_profile::{CategoryPairHandle, ThreadHandle};
 use indicatif::{ProgressBar, ProgressStyle};
 use mimalloc::MiMalloc;
 use object::read::macho::FatArch;
@@ -36,6 +37,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let file_kind = FileKind::parse(&data[..]).unwrap();
 
+    let mut profile = Profile::new(
+        "size-profiler",
+        ReferenceTimestamp::from_millis_since_unix_epoch(0.),
+        SamplingInterval::from_hz(1000.),
+    );
+
+    let process = profile.add_process(file_name, 0, Timestamp::from_millis_since_reference(0.));
+    let thread = profile.add_thread(process, 0, Timestamp::from_millis_since_reference(0.), true);
+    profile.set_thread_samples_weight_type(thread, WeightType::Bytes);
+    let category = CategoryHandle::OTHER.into();
+
+    let root_s = profile.intern_string("(root)");
+    let root_frame = profile.intern_frame(
+        thread,
+        FrameInfo {
+            frame: Frame::Label(root_s),
+            category_pair: category,
+            flags: FrameFlags::empty(),
+        },
+    );
+    let root_stack = profile.intern_stack(thread, None, root_frame);
+
+    let config = SymbolManagerConfig::default()
+        .respect_nt_symbol_path(true)
+        .breakpad_symbols_server(
+            "https://symbols.mozilla.org/try/",
+            "./breakpad-symbol-cache/",
+        )
+        .breakpad_symindex_cache_dir("./breakpad-symindex-cache/");
+    let symbol_manager = SymbolManager::with_config(config);
+
     // If we got a fat binary, pick the first member.
     let data = if dbg!(file_kind) == FileKind::MachOFat32 {
         let member = MachOFatFile32::parse(&data[..])
@@ -63,6 +95,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         SymbolManager::library_info_for_binary_at_path(Path::new(path), disambiguator.clone())
             .await
             .unwrap();
+
+    let symbol_map = symbol_manager
+        .load_symbol_map_for_binary_at_path(Path::new(path), disambiguator)
+        .await
+        .unwrap();
+
+    process_binary(
+        &mut profile,
+        thread,
+        root_stack,
+        &object_file,
+        lib_info,
+        symbol_map,
+        category,
+    )
+    .await;
+    let output_file = std::fs::File::create("output.json").unwrap();
+    let writer = std::io::BufWriter::new(output_file);
+    serde_json::to_writer(writer, &profile).unwrap();
+
+    Ok(())
+}
+
+async fn process_binary(
+    profile: &mut Profile,
+    thread: ThreadHandle,
+    root_stack: StackHandle,
+    object_file: &File<'_>,
+    lib_info: wholesym::LibraryInfo,
+    symbol_map: wholesym::SymbolMap,
+    category: CategoryPairHandle,
+) {
     let lib = LibraryInfo {
         name: lib_info.name.unwrap(),
         debug_name: lib_info.debug_name.unwrap(),
@@ -74,42 +138,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         symbol_table: None,
     };
 
-    let config = SymbolManagerConfig::default()
-        .respect_nt_symbol_path(true)
-        .breakpad_symbols_server(
-            "https://symbols.mozilla.org/try/",
-            "./breakpad-symbol-cache/",
-        )
-        .breakpad_symindex_cache_dir("./breakpad-symindex-cache/");
-    let symbol_manager = SymbolManager::with_config(config);
-    let symbol_map = symbol_manager
-        .load_symbol_map_for_binary_at_path(Path::new(path), disambiguator)
-        .await
-        .unwrap();
+    let base_addr = relative_address_base(object_file);
 
-    let mut profile = Profile::new(
-        "size-profiler",
-        ReferenceTimestamp::from_millis_since_unix_epoch(0.),
-        SamplingInterval::from_hz(1000.),
-    );
     let _lib = profile.add_lib(lib);
-    let process = profile.add_process(file_name, 0, Timestamp::from_millis_since_reference(0.));
-    let thread = profile.add_thread(process, 0, Timestamp::from_millis_since_reference(0.), true);
-    profile.set_thread_samples_weight_type(thread, WeightType::Bytes);
-    let category = CategoryHandle::OTHER.into();
-
-    let base_addr = relative_address_base(&object_file);
-
-    let root_s = profile.intern_string("(root)");
-    let root_frame = profile.intern_frame(
-        thread,
-        FrameInfo {
-            frame: Frame::Label(root_s),
-            category_pair: category,
-            flags: FrameFlags::empty(),
-        },
-    );
-    let root_stack = profile.intern_stack(thread, None, root_frame);
 
     let unknown_path_str = profile.intern_string("<unknown path>");
     let unknown_path_frame = profile.intern_frame(
@@ -167,7 +198,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut weight = 0;
         for addr in section_start_rel..section_end_rel {
             if addr & 0xffff == 0 {
-                pb.set_position((addr - section_start_rel) as u64);
+                pb.set_position(addr - section_start_rel);
             }
 
             let addr_info = symbol_map
@@ -277,9 +308,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         pb.finish_with_message("Section processed");
     }
-    let output_file = std::fs::File::create("output.json").unwrap();
-    let writer = std::io::BufWriter::new(output_file);
-    serde_json::to_writer(writer, &profile).unwrap();
-
-    Ok(())
 }
